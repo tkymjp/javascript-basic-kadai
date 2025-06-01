f2c454a291291d5deb4481a36d51204e59ff794f
tkymjp <canonbass@gmail.com>
2025-06-01 11:41:59 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+const japaneseDate =`${year}年${month}月${day}日`;
+console.log(japaneseDate);
